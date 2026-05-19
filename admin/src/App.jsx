61ec766a0b292e354936c6570eab3872286f0357
prasaman <wishbe36@gmail.com>
2026-05-19 22:45:34 +0545

    admin login page completed

---
+++ admin/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+  import { ToastContainer, toast } from 'react-toastify';
+
+import { Route,Routes } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/list'
+import Orders from './pages/orders'
+import Login from './components/Login'
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+
+
+
+const App = () => {
+
+  const [token,setToken] = useState('')
+
+  return (
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer/>
+
+      {token === '' ? <Login setToken={setToken} /> :
+      <>
+     
+    <Navbar />
+     <hr />
+     <div className='flex w-full '>
+      <Sidebar />
+      <div className='w-[80%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+        <Routes>
+          <Route path = '/add' element = {<Add />}  />
+          <Route path = '/list' element = {<List />}  />
+          <Route path = '/orders' element = {<Orders />}  />
+         
+        </Routes>
+
+      </div>
+
+     </div>
+    </>
+    }
+
+    </div>
+  )
+}
+
+export default App
