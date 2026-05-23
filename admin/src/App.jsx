328fb5f98a5e9f6cee9b58f2d3e8ef95bbaeefd2
prasaman <wishbe36@gmail.com>
2026-05-23 22:53:17 +0545

    add product page completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
   import { ToastContainer, toast } from 'react-toastify';
@@ -16,7 +16,12 @@ export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
 
-  const [token,setToken] = useState('')
+  const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'')
+
+  useEffect(() => {
+    localStorage.setItem('token',token)
+    
+},[token])
 
   return (
     <div className='bg-gray-50 min-h-screen'>
@@ -25,15 +30,15 @@ const App = () => {
       {token === '' ? <Login setToken={setToken} /> :
       <>
      
-    <Navbar />
+    <Navbar setToken = {setToken} />
      <hr />
      <div className='flex w-full '>
       <Sidebar />
       <div className='w-[80%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
         <Routes>
-          <Route path = '/add' element = {<Add />}  />
-          <Route path = '/list' element = {<List />}  />
-          <Route path = '/orders' element = {<Orders />}  />
+          <Route path = '/add' element = {<Add token = {token}/>}  />
+          <Route path = '/list' element = {<List token = {token}/>}  />
+          <Route path = '/orders' element = {<Orders token = {token}/>}  />
          
         </Routes>
 
